ed0329f764d070cd4dcd2d23424fe75c0422f46d
Nina Madeira <nina.madeira@gmail.com>
2023-05-03 22:37:21 -0300

    subindo pedidosy e gatewayy

---
+++ pediddosy/src/main/java/br/com/mfood/pediddosy/dto/StatusDto.java
@@ -0,0 +1,15 @@
+package br.com.mfood.pediddosy.dto;
+
+import br.com.mfood.pediddosy.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusDto {
+    private Status status;
+}
